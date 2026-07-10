386e9648bd7f14c2b359d3f76c296c4998e27c6b
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-07-10 08:55:12 -0500

    cambio de metodo put -> patch

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/users", userController.get);
 router.get("/users/:id", userController.getById);
 router.post("/users", userController.create);
-router.put("/users/:id", userController.update);
+router.patch("/users/:id", userController.update);
 router.delete("/users/:id", userController.delete);
 
 export { router };
